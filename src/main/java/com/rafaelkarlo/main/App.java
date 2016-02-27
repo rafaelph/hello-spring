bbfeede6addc75ed0f77cd5920bf1013394784cb
Rafael Delos Santos <rkdelossantos@gmail.com>
2016-02-27 12:35:52 +1100

    Using application context with different configurations in main

--- src/main/java/com/rafaelkarlo/main/App.java
+++ src/main/java/com/rafaelkarlo/main/App.java
@@ -1,7 +1,20 @@
 package com.rafaelkarlo.main;
 
+import com.rafaelkarlo.configuration.CDPlayerConfig;
+import com.rafaelkarlo.objects.MediaPlayer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        ApplicationContext contextWithXMLConfiguration = new ClassPathXmlApplicationContext("springconfig.xml");
+        MediaPlayer mediaPlayer = (MediaPlayer) contextWithXMLConfiguration.getBean("cdPlayer");
+        mediaPlayer.play();
+
+        ApplicationContext contextWithAnnotatedConfiguration = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
+        mediaPlayer = contextWithAnnotatedConfiguration.getBean(MediaPlayer.class);
+        mediaPlayer.play();
     }
 }
